a79dfe3fb437a75d8ecaaa802ee411da7bc86f62
barry.de.vries123@gmail.com <barry.de.vries123@gmail.com>
2019-06-12 13:11:02 +0200

    updated parser to write correct output to file

--- moodanalysis/parsers/data_utrecht_parser.py
+++ moodanalysis/parsers/data_utrecht_parser.py
@@ -3,38 +3,42 @@ The data is added and put into a list of lists (data) which we can use for train
 algorithm for mood analysis. Another Python file supplied by Barry scrapes the spotify API for audio-features
 related to these tracks. In this way we can compare interviewees opinions to spotify's supplied parameters."""
 import csv
+from numpy import add
+
+data = []
 
 with open('./../machinelearning/data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     iterrows = iter(csv_reader)
     next(iterrows)
     numericdata = []
+    #Iterate over all columns except text and map strings -> int
     for row in csv_reader:
         del row[1]
-        del row[15]
-        for i in range(15):
+        for i in range(6):
+            del row[10]
+        for i in range(10):
             row[i] = int(row[i])
         numericdata.append(row)
-    data = []
     list_count = 0
     new_data = [0 for i in range(13)]
     numberofresponses = [0 for i in range(401)]
+# Add together responses on same song
     for row in numericdata:
+        gradesum = sum(row[1:])
+        if gradesum == 0:
+            gradesum = 1
+
         if row[0] > list_count:
             if(numberofresponses[list_count - 1] >= 20):
-                data.append(new_data)
-                new_data = [0 for i in range(13)]
+                data.append([row[0]] + [value / numberofresponses[list_count - 1] for value in new_data])
+            new_data = [value / gradesum for value in row[1:]]
             list_count += 1
             numberofresponses[list_count] += 1
         else:
+            new_data = add(new_data,[value / gradesum for value in row[1:]])
             numberofresponses[list_count-1] += 1
-            for i in range(13):
-                new_data[i] += row[i+1]
-    for i in range(len(data)):
-        print("Song: ")
-        print(data[i][0])
-        print("Data gathered: ")
-        print(data[i])
-        print("Number of responses: ")
-        print(numberofresponses[i])
-        print("\n\n")
+
+with open('./../machinelearning/moods_20.csv', 'w') as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=',')
+    csv_writer.writerows(data)
