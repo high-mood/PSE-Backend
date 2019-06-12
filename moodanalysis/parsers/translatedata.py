f4e640c49fc9835894ca113a5b1d140606320e83
Jonasvdham <jonasvdham@gmail.com>
2019-06-12 12:31:35 +0200

    UU parser 2

---
+++ moodanalysis/parsers/translatedata.py
@@ -0,0 +1,21 @@
+import csv
+from numpy import dot
+
+moodmap = [(9,8),(3,5),(-7,5),(-2,5),(-8,1),(9,2),(6,9),(7,-4),(-7,-8)]
+energyvector = [9, 3, -7, -2, -8, 9, 6, 7, -7]
+happyvector = [8, 5, 5, 5, 1, 2, 9, -4, -8]
+with open('testnorm.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',') 
+    outputdata = []
+    
+    for row in csv_reader:
+        for i in range(len(row)):
+            row[i] = float(row[i])
+        outputdata.append([row[0], dot(row[1:], energyvector), dot(row[1:], happyvector)])
+
+print(outputdata)
+
+with open('moods.csv', "w") as out:
+    output = csv.writer(out)
+    output.writerows(outputdata)
+    
