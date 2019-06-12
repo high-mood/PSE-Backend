0e5cdb1b720e93834f2fdb3f635cfa164252b616
barry.de.vries123@gmail.com <barry.de.vries123@gmail.com>
2019-06-12 12:16:16 +0200

    minor bug fixing

--- moodanalysis/random_forest.py
+++ moodanalysis/random_forest.py
@@ -17,25 +17,33 @@ def read_data():
 
         # Skip headers.
         next(rows_iter)
-
         for rows in rows_iter:
             if(np.random.uniform(0, 1) <= TRAIN_RATIO):
                 train_set.append(rows)
             else:
                 test_set.append(rows)
-        
+
     return train_set,test_set
 
 
-def somehypenameboi(train_set, test_set):
-    clf = RandomForestClassifier(n_jobs=2, random_state=0)
+def random_forest(train_set, test_set):
+    clf = RandomForestClassifier(n_jobs=2, n_estimators=10)
     clf.fit([row[1:14] for row in train_set], [row[14] for row in train_set])
 
     result = clf.predict([row[1:14] for row in test_set])
+    succes = 0
+    failure = 0
     
-    for item in zip(result, [row[14] for row in train_set]):
-        print( "found: " + str(item[0]) + " correct is: " + str(item[1]))
+    for item in zip(result, [row[14] for row in test_set]):
+        if(item[0] == item[1]):
+            succes += 1
+        else:
+            failure += 1
+            print("difference is " + str(int(item[1]) - int(item[0])))
 
+
+    print("succes: " + str(succes))
+    print("failure: " + str(failure))
 
 
 if __name__ == "__main__":
@@ -43,4 +51,4 @@ if __name__ == "__main__":
     np.random.seed(0)
     train_set, test_set = read_data()
 
-    somehypenameboi(train_set, test_set)
+    random_forest(train_set, test_set)
