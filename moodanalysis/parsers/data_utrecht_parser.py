20edae4e01a016b8ce99d0b094f52d98bec8350e
barry.de.vries123@gmail.com <barry.de.vries123@gmail.com>
2019-06-24 15:17:09 +0200

    added comments

--- moodanalysis/parsers/data_utrecht_parser.py
+++ moodanalysis/parsers/data_utrecht_parser.py
@@ -18,7 +18,7 @@ with open('./../machinelearning/data.csv') as csv_file:
     next(iterrows)
     numericdata = []
     
-    #Iterate over all columns except text and map strings -> int
+    # Iterate over all columns except text and map strings -> int.
     for row in csv_reader:
         del row[1]
         for i in range(6):
@@ -28,23 +28,31 @@ with open('./../machinelearning/data.csv') as csv_file:
         numericdata.append(row)
 
     list_count = 0
-    new_data = [0 for i in range(13)]
-    numberofresponses = [0 for i in range(401)]
+    new_data = [0 for _ in range(13)]
+    numberofresponses = [0 for _ in range(400)]
 
-    # Add together responses on same song
+    # Add together responses on same song.
     for row in numericdata:
+
+        # Compute the factor to use for normalisation.
         gradesum = sum(row[1:])
         if gradesum == 0:
             gradesum = 1
 
+        # Check if a new track id is found.
         if row[0] > list_count:
+            # Check if the number of responses surpasses the threshold.
             if(numberofresponses[list_count - 1] >= int(sys.argv[1])):
+                # Devide track data by the number of responses, then append to results.
                 data.append([row[0]] + [value / numberofresponses[list_count - 1] for value in new_data])
+            
+            # Reinitialize the track data list.
             new_data = [value / gradesum for value in row[1:]]
             list_count += 1
             numberofresponses[list_count] += 1
         else:
-            new_data = add(new_data,[value / gradesum for value in row[1:]])
+            # Add weighted data to track data.
+            new_data = add(new_data, [value / gradesum for value in row[1:]])
             numberofresponses[list_count-1] += 1
 
 with open('./../machinelearning/moods_' + sys.argv[1] +'.csv', 'w') as csv_file:
