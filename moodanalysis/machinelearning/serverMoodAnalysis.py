620c1b0cb3b7b9c14652671d15dea0abc43d85f8
barry.de.vries123@gmail.com <barry.de.vries123@gmail.com>
2019-06-24 14:32:28 +0200

    comments added

--- moodanalysis/machinelearning/serverMoodAnalysis.py
+++ moodanalysis/machinelearning/serverMoodAnalysis.py
@@ -4,8 +4,11 @@ import numpy as np
 
 ''' Mood classification for server, requires the .joblib files imported hereunder.'''
 
+# Load trained models from joblib files
 E_est = load('Trained-Energy.joblib')
 H_est = load('Trained-Happiness.joblib')
+
+# List of track features.
 features = ["mode", "time_signature", "acousticness",
         "danceability", "energy", "instrumentalness", "liveness", "loudness",
         "speechiness", "valence", "tempo"]
@@ -14,6 +17,7 @@ def analyseMood(songs):
     input = []
     songtitles = []
     output = []
+
     for song in songs:
         # Make list of titles.
         songtitles.append(song['songid'])
@@ -22,13 +26,17 @@ def analyseMood(songs):
         for feature in features:
             inputdata.append(song[feature])
         input.append(inputdata)
+
     EnergyPredictions = E_est.predict(input)
     HappinessPredictions = H_est.predict(input)
+
+    # Fill output list with dicts of song id's, energy an happiness values.
     for i in range(len(songtitles)):
         outputdata = {}
         outputdata['songid'] = songtitles[i]
         outputdata['energy'] = EnergyPredictions[i]
         outputdata['happiness'] = HappinessPredictions[i]
         output.append(outputdata)
+
     return output
         
